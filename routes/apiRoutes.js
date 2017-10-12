a1206085b9459a9ba14900878bebbc36e8935563
Andy Bonner <abonner@duke.edu>
2017-10-12 10:43:55 -0400

    ID and view routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,9 +3,19 @@ const router = require("express").Router();
 // import controller, which imports model(s)
 const articlesController = require("../controllers/articlesController");
 
+// actions not needing a specific ID:
 router.route("/api/saved")
   .get(articlesController.findAll)
-  .post(articlesController.create)
+  .post(articlesController.create);
 
+// actions needing a specific ID:
+router.route("/:id")
+  .delete(articlesController.remove);
+
+// All views are handled by React:
+// TODO: check, is this ok or does it need router.use?
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+})
 
 module.exports = router;
