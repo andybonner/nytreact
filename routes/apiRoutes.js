e2c7c66fdebbb5f5796b111a931a66b12bf44d54
Andy Bonner <abonner@duke.edu>
2017-10-12 10:24:36 -0400

    API findall and create routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,11 @@
 const axios = require("axios");
 const router = require("express").Router();
+// import controller, which imports model(s)
+const articlesController = require("../controllers/articlesController");
+
+router.route("/api/saved")
+  .get(articlesController.findAll)
+  .post(articlesController.create)
+
+
+module.exports = router;
